1e3d8af9f403425507e8672799684d14ba2211ee
RWHA05 <94277313+RWHA05@users.noreply.github.com>
2024-11-12 00:19:08 -0600

    Modular files added

---
+++ test_ColorPair.h
@@ -0,0 +1,27 @@
+#ifndef _TEST_COLORPAIR_H_
+#define _TEST_COLORPAIR_H_
+
+#include "get_ColorPair_func.h"
+
+void testNumberToPair(int pairNumber,
+    TelCoColorCoder::MajorColor expectedMajor,
+    TelCoColorCoder::MinorColor expectedMinor)
+{
+    TelCoColorCoder::ColorPair colorPair =
+        TelCoColorCoder::GetColorFromPairNumber(pairNumber);
+    std::cout << "Got pair " << colorPair.ToString() << std::endl;
+    assert(colorPair.getMajor() == expectedMajor);
+    assert(colorPair.getMinor() == expectedMinor);
+}
+
+void testPairToNumber(
+    TelCoColorCoder::MajorColor major,
+    TelCoColorCoder::MinorColor minor,
+    int expectedPairNumber)
+{
+    int pairNumber = TelCoColorCoder::GetPairNumberFromColor(major, minor);
+    std::cout << "Got pair number " << pairNumber << std::endl;
+    assert(pairNumber == expectedPairNumber);
+}
+
+#endif //_TEST_COLORPAIR_H_
